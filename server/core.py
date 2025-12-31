9215eff1b421d3482d7be226bce5984fd0ff71d4
Kainan H. <kainanhenriq18@hotmail.com>
2025-12-31 11:18:15 -0300

    Adicionado função para obter dados de um empresa

--- server/core.py
+++ server/core.py
@@ -41,14 +41,76 @@ class APICore(Database):
             "auth_key": auth_key
         }
 
-    def company_get(self, company_id):
-        return self.cmd(
-            "SELECT * FROM companies WHERE company_id = ?",
+    def company_get_data(self, company_id):
+        status_comp, data_comp = self.cmd(
+            """
+                SELECT 
+                    company_name, auth_key, status, created_at
+                FROM 
+                    companies 
+                WHERE 
+                    company_id = ?
+            """,
             (company_id,),
             action="fetchone"
         )
 
+        status_print, data_print = self.cmd(
+            """
+                SELECT 
+                    printer_id, hostname, last_ip, status, status_payload, last_connected_at, created_at 
+                FROM 
+                    printers 
+                WHERE 
+                    company_id = ?
+            """,
+            arguments=(company_id,),
+            action="fetchall"
+        )
+
+        data_result = {
+            "company": {
+                "id": company_id,
+                "name": data_comp[0],
+                "auth_key": data_comp[1],
+                "status": data_comp[2],
+                "created_at": data_comp[3]
+            },
+            "printers" : []
+        }
+
+        for printer in data_print:
+            data_result['printers'].append(
+                {
+                    "id": data_print[0],
+                    "hostname": data_print[1],
+                    "last_ip": data_print[2],
+                    "status": data_print[3],
+                    "status_payload": data_print[4],
+                    "last_connection": data_print[5],
+                    "created_at": data_print[6] 
+                }
+            )
+
+        if not status_comp:
+            return status_comp, data_comp
+
+        elif not status_print:
+            return status_print, data_print
+
+        return True, data_result
+
+# +=====================+
+# |                     |
+# |  _____         _    |
+# | |_   _|__  ___| |_  |
+# |   | |/ _ \/ __| __| |
+# |   | |  __/\__ \ |_  |
+# |   |_|\___||___/\__| |
+# |                     |
+# +=====================+
 if __name__ == "__main__":
     api_core = APICore()
-    print(api_core.company_register("Thenorian"))
-    print(api_core.companies_all_list())
+    # print(api_core.company_register("Thenorian"))
+    # print(api_core.companies_all_list())
+    print(api_core.company_get_data("92a6259b-80c0-4633-be2a-d81a44e85a6e"))
